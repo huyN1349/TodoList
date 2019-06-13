f3501adafd37eb131e5e1507f912cc6bc15573f3
HuyN1349 <huy.nguyen1349@gmail.com>
2019-06-12 18:58:33 -0700

    Added customized list of Todo List.

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// const items = ["Buy Food", "Cook Food", "Eat Food"];
-// const workItems = [];
-
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -27,107 +23,82 @@ const itemSchema = new Schema({
 
 const Item = mongoose.model("Item", itemSchema);
 
-
-
-
-// const homeItems = Item.find({},function(err, items){
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     //mongoose.connection.close();
-//     console.log(items);
-//       return items.name;
-//   }
-// });
-// //
-const workItems = Item.find({type: "Work"},function(err, items){
-  if (err) {
-    console.log(err);
-  } else {
-    //mongoose.connection.close();
-    items.forEach(function(item){
-      return item.name;
-    })
-  }
-});
-//------Index Page-----//
-app.get("/", function(req, res) {
-  let day = date.getDate();
-  Item.find({type:"Home"},function(err, homeItems){
-    if (homeItems.length === 0) {
-      const item1 = new Item({name: "Welcome to your Todo List!", type: "Home"});
-      const item2 = new Item({name: "Hit the + button to add a new item.", type:"Home"});
-      const item3 = new Item({name: "Hit this to delete an item.", type:"Home"});
+//Render a page
+app.get("/", function(req,res){
+  Item.find({type:"Default"}, function(err,foundItems){
+    if (foundItems.length === 0){
+      const item1 = new Item({name: "Welcome to your Todo List!", type: "Default"});
+      const item2 = new Item({name: "Hit the + button to add a new item.", type: "Default"});
+      const item3 = new Item({name: "Hit this to delete an item.", type: "Default"});
       Item.insertMany([item1,item2,item3], function(err){
-        if (err){
+        if (err) {
           console.log(err);
-        } else{
-          console.log("Successfully save items.")
+        } else {
+          console.log("Successfully save items");
         }
       });
       res.redirect("/");
     } else {
       res.render("list", {
-          listTitleEJS: day,
-          itemsEJS: homeItems,
-        });
-    };
-  });
-})
-
-app.post("/", function(req, res) {
-  if (req.body.list === "Work") {
-    Item.create({name:req.body.newItem, type:"Work"},function(err){
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Successfully save a Work item.")
-      }
-    });
-    res.redirect("/work");
-  } else {
-    Item.create({name:req.body.newItem, type:"Home"},function(err){
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Successfully save a Home item.")
-      }
-    });
-    res.redirect("/");
-  };
-})
-
-app.post("/delete", function(req,res){
-  console.log(req.body.checkbox);
-  Item.deleteOne({_id: req.body.checkbox}, function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Successfully delete a completed Home item.")
-      res.redirect("/");
+        listTitleEJS: "Default Todo-List",
+        itemsEJS: foundItems,
+      });
     }
   })
 })
-//------Work Page------//
-app.get("/work", function(req, res) {
-  Item.find({type:"Work"},function(err,workItems){
-    res.render("list", {
-      listTitleEJS: "Work List",
-      itemsEJS: workItems,
-    });
+
+app.get("/:todoType", function(req,res){
+  Item.find({type:req.params.todoType}, function(err,foundItems){
+    //console.log(req.params.todoType);
+    if (foundItems.length === 0){
+      const item1 = new Item({name: "Welcome to your Todo List!", type: req.params.todoType});
+      const item2 = new Item({name: "Hit the + button to add a new item.", type: req.params.todoType});
+      const item3 = new Item({name: "Hit this to delete an item.", type: req.params.todoType});
+      Item.insertMany([item1,item2,item3], function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully save items");
+        }
+      });
+      res.redirect("/" + req.params.todoType);
+    } else {
+      res.render("list", {
+        listTitleEJS: req.params.todoType + " Todo-List",
+        itemsEJS: foundItems,
+      });
+    }
   });
 });
 
-app.post("/work", function(req, res) {
-  Item.create({name:req.body.newItem, type:"Work"},function(err){
+//Save a new item
+app.post("/", function(req, res) {
+  Item.create({name:req.body.newItem, type:req.body.list}, function(err){
     if (err) {
       console.log(err);
     } else {
-      console.log("Successfully save a Work item.")
+      console.log("Successfully save an item.")
+    }
+  })
+    if (req.body.list === "Default"){
+      res.redirect("/");
+    } else {
+      res.redirect("/" + req.body.list);
+    }
+});
+
+//Delete an item
+app.post("/delete", function(req,res){
+  Item.findOneAndDelete({_id:req.body.checkbox}, function(err, foundItem){
+    if (err){
+      console.log(err);
+    } else {
+      //console.log(foundItem.type[0]);
+      res.redirect("/" + foundItem.type[0]);
     }
   });
-  res.redirect("/");
 })
+
 
 //-----About Page-----//
 app.get("/about", function(req,res) {
