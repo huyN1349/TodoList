c35b7b109f70cc39056ba10d94d18a2c4c44e59f
HuyN1349 <huy.nguyen1349@gmail.com>
2019-07-21 01:04:35 -0700

    Refactoring codes. Adding relationship in NoSQL.

--- app.js
+++ app.js
@@ -40,128 +40,105 @@ const List = mongoose.model("List", listSchema);
 
 //Render a page
 app.get(["/", "/[iI]ndex", "/[hH]ome"], function(req, res) {
-  List.find({urlName: "home"}, function(err, foundList) {
-    if (err) {
-      console.log(err);
+  List.find({
+    urlName: "home"
+  }, function(err, foundList) {
+    if (foundList.length === 0) {
+      const list = new List({
+        urlName: "home",
+        item: defaultItems,
+      });
+      list.save();
+      res.redirect("/");
     } else {
-      if (foundList.length === 0) {
-        const list = new List({
-          urlName: "home",
-          item: defaultItems,
+      if (foundList[0].item.length === 0) {
+        Item.insertMany(defaultItems, function(err) {
+          console.log("Successfully save items");
         });
-        list.save();
+        res.redirect("/");
       } else {
-        //console.log(foundList[0].item);
-        if (foundList[0].item.length === 0) {
-          Item.insertMany(defaultItems, function(err) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log("Successfully save items");
-            }
-          });
-          res.redirect("/");
-        } else {
-          res.render("list", {
-            listTitleEJS: "Home Todo List",
-            itemsEJS: foundList[0].item,
-          });
-        }
+        res.render("list", {
+          listTitleEJS: "Home",
+          itemsEJS: foundList[0].item,
+        });
       }
     }
   })
 })
 
-
-app.get("/:urlName", function(req,res){
-  List.find({urlName: req.params.urlName.toLowerCase()}, function(err, foundList) {
-    if (err) {
-      console.log(err);
+app.get(["/:urlName"], function(req, res) {
+  List.find({
+    urlName: req.params.urlName.toLowerCase()
+  }, function(err, foundList) {
+    if (foundList.length === 0) {
+      const list = new List({
+        urlName: req.params.urlName.toLowerCase(),
+        item: defaultItems,
+      });
+      list.save();
+      res.redirect("/" + req.params.urlName.toLowerCase());
     } else {
-      if (foundList.length === 0) {
-        const list = new List({
-          urlName: req.params.urlName.toLowerCase(),
-          item: defaultItems,
+      if (foundList[0].item.length === 0) {
+        Item.insertMany(defaultItems, function(err) {
+          console.log("Successfully save items");
         });
-        list.save();
+        res.redirect("/" + req.params.urlName.toLowerCase());
       } else {
-        //console.log(foundList[0].item);
-        if (foundList[0].item.length === 0) {
-          Item.insertMany(defaultItems, function(err) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log("Successfully save items");
-            }
-          });
-          res.redirect("/" + req.params.urlName.toLowerCase());
-        } else {
-          res.render("list", {
-            listTitleEJS: req.params.urlName.charAt(0).toUpperCase() + req.params.urlName.slice(1).toLowerCase() + " Todo List",
-            itemsEJS: foundList[0].item,
-          });
-        }
+        res.render("list", {
+          listTitleEJS: req.params.urlName.charAt(0).toUpperCase() + req.params.urlName.slice(1).toLowerCase(),
+          itemsEJS: foundList[0].item,
+        });
       }
     }
-  })
-})
-//   Item.find({type:req.params.urlName}, function(err,foundItems){
-//     //console.log(req.params.todoType);
-//     if (foundItems.length === 0){
-//       const item1 = new Item({name: "Welcome to your Todo List!"});
-//       const item2 = new Item({name: "Hit the + button to add a new item."});
-//       const item3 = new Item({name: "Hit this to delete an item."});
-//       Item.insertMany([item1,item2,item3], function(err){
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           console.log("Successfully save items");
-//         }
-//       });
-//       res.redirect("/" + req.params.urlName);
-//     } else {
-//       res.render("list", {
-//         listTitleEJS: req.params.urlName + " Todo-List",
-//         itemsEJS: foundItems,
-//       });
-//     }
-//   });
-// });
+  });
+});
+
 
 //Save a new item
 app.post("/", function(req, res) {
   console.log(req.body.list.toLowerCase());
   console.log(req.body.newItem);
-  List.updateOne(
-    {urlName: req.body.list.toLowerCase()},
-    {
-      $set: {item: req.body.newItem},
-      //urlName: req.body.list.toLowerCase(),
 
-    },
-    {upsert: true},
-)
+  List.findOne({
+    urlName: req.body.list.toLowerCase()
+  }, function(err, foundList) {
+    const item = new Item({
+      name: req.body.newItem,
+    });
+    foundList.item.push(item);
+    foundList.save();
 
-  if (req.body.list.toLowerCase() === "home") {
-    res.redirect("/");
-  } else {
-    res.redirect("/" + req.body.list.toLowerCase());
-  }
+    if (req.body.list.toLowerCase() === "home") {
+      res.redirect("/");
+    } else {
+      res.redirect("/" + req.body.list.toLowerCase());
+    }
+  })
 });
 
 //Delete an item
 app.post("/delete", function(req, res) {
-  Item.findOneAndDelete({
-    _id: req.body.checkbox
-  }, function(err, foundItem) {
-    if (err) {
-      console.log(err);
+  const urlName = req.body.urlName.toLowerCase();
+  const itemID = new mongoose.Types.ObjectId(req.body.checkbox);
+  console.log(req.body.checkbox);
+  console.log(urlName);
+  List.updateOne({
+    "urlName": urlName
+  }, {
+    $pull: {
+      "item": {
+        "_id": itemID
+      }
+    }
+  }, function(err, result) {
+    console.log(result);
+    if (urlName === "home") {
+      res.redirect("/");
     } else {
-      //console.log(foundItem.type[0]);
-      res.redirect("/" + foundItem.type[0]);
+      res.redirect("/" + urlName)
     }
   });
-})
+});
 
 
 //-----About Page-----//
